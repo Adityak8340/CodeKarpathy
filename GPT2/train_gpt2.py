ec5ea8d80c1f193f0af2b9de6f2d5d58ac35ebe3
Adityak8340 <adityak8340@gmail.com>
2025-03-03 16:04:09 +0530

    using old dataset

--- GPT2/train_gpt2.py
+++ GPT2/train_gpt2.py
@@ -17,6 +17,7 @@ class CausalSelfAttention(nn.Module):
             self.c_attn = nn.Linear(config.n_embd, config.n_embd * 3)
             # output projection
             self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+            self.c_proj.NANOGPT_SCALE_INIT = 1
             # regularization
             self.n_head = config.n_head
             self.n_embd = config.n_embd
@@ -51,7 +52,8 @@ class MLP(nn.Module):
             self.c_fc = nn.Linear(config.n_embd, config.n_embd * 4)
             self.gelu = nn.GELU(approximate='tanh')
             self.c_proj = nn.Linear(config.n_embd * 4, config.n_embd)
-    
+            self.c_proj.NANOGPT_SCALE_INIT = 1
+
         def forward(self, x):
             x = self.c_fc(x)
             x = self.gelu(x)  
@@ -94,7 +96,24 @@ class GPT(nn.Module):
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)                           # Language Model Head (Linear Layer)
     
-    def forward(self, idx):
+        # weight sharing scheme
+        self.transformer.wte.weight = self.lm_head.weight
+
+        # init params
+        self.apply(self._init_weights)
+    
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            std = 0.02
+            if hasattr(module, 'NANOGPT_SCALE_INIT'):
+                std *= (2 * self.config.n_layer) ** -0.5
+            torch.nn.init.normal_(module.weight, mean=0.0, std=std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+    def forward(self, idx, targets=None):
         # idx is of shape (B, T)
         B, T = idx.size()
         assert T <= self.config.block_size, "Cannot forward, model block size is exhausted."
@@ -109,7 +128,11 @@ class GPT(nn.Module):
         # forward the final layer norm and the classifier
         x = self.transformer.ln_f(x)
         logits = self.lm_head(x)                                          # (B, T, vocab_size)
-        return logits
+        # loss
+        loss = None
+        if targets is not None:
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+        return logits, loss
 
     @classmethod
     def from_pretrained(cls, model_type):
@@ -161,6 +184,38 @@ class GPT(nn.Module):
         return model
 
 # -----------------------------------------------------------------------------
+import tiktoken
+
+class DataLoaderLite:
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
+
+        # at init load tokens from disk and store them in RAM
+        with open('input.txt', 'r', encoding='utf-8') as f:
+            text = f.read()
+        enc = tiktoken.get_encoding('gpt2')
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens) # (N,)
+        print(f"Loaded {len(self.tokens)} tokens")
+        print(f"1 epoch = {len(self.tokens) // (B*T)} batches")
+
+        # state
+        self.current_position = 0
+
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position:self.current_position + B*T+1] # (B*T+1,)
+        x = buf[:-1].view(B, T) # input
+        y = buf[1:].view(B, T) # output
+        # advance the position in tensor
+        self.current_position += B*T
+        # if loading the next batch would overrun the tokens, reset the position
+        if self.current_position + B*T + 1 > len(self.tokens):
+            self.current_position = 0
+        return x, y
+
+# -----------------------------------------------------------------------------
 # attempt to autodetect the device
 device = "cpu"
 if torch.cuda.is_available():
@@ -168,6 +223,29 @@ if torch.cuda.is_available():
 elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
     device = "mps"
 print(f"Using device: {device}")
+
+torch.manual_seed(1337)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(1337)
+
+train_loader = DataLoaderLite(B=4, T=32)
+
+# get logits
+model = GPT(GPTConfig())
+model.to(device)
+
+# optimize!
+optimizer = torch.optim.Adam(model.parameters(), lr=3e-4)
+for i in range(50):
+    x, y = train_loader.next_batch()
+    x, y = x.to(device), y.to(device)
+    optimizer.zero_grad()
+    logits, loss = model(x, y)
+    loss.backward()
+    optimizer.step()
+    print(f"step {i}, loss: {loss.item()}")
+
+import sys; sys.exit(0)
 
 # model = GPT.from_pretrained('gpt2')
 model = GPT(GPTConfig())
